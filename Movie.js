bc7c04cba58843408a84c8ce841551c635cb6fcb
07emanuelle <e.celestino@escola.pr.gov.br>
2024-11-28 08:45:30 -0300

    Create Movie.js

---
+++ Movie.js
@@ -0,0 +1,307 @@
+Ação
+
+{
+title: “Os incríveis”,
+genre: Ação,
+ageRating: livre,
+synopsis: “Depois de o governo banir o uso de superpoderes, o maior herói do planeta, o Sr. Incrível, vive de forma pacata com sua família. Apesar de estar feliz com a vida doméstica, o Sr. Incrível ainda sente falta dos tempos em que viveu como super-herói, e sua grande chance de entrar em ação novamente surge quando um velho inimigo volta a atacar. Só que agora ele precisa contar com a ajuda de toda a família para vencer o vilão.”,
+Trailer: “https://youtu.be/ftBvKUAagnU?si=xyVhw2LQcO6qiYmo”
+},
+{
+title: “O espetacular homem-aranha”,
+genre: :Ação”,
+ageRating: 10 anos,
+synopsis: “O jovem Peter Parker quer saber mais sobre sua origem. Ele encontra uma pasta que pertenceu ao seu pai e tenta descobrir por que seus pais desapareceram. A sua busca o leva a Oscorp e ao dr. Curt Connors, que tem como alter ego o letal Lagarto.”,
+Trailer: “https://youtu.be/6nzNdpJ3qMo?si=7nYO7Uw_4dPpWK6b”
+},
+{
+title: “Esquadrão Suicida”,
+genre: “Ação”,
+ageRating: 12 anos,
+synopsis: “Um time dos mais perigosos e encarcerados supervilões são contratados por uma agência secreta do governo, para combater uma poderosa entidade. No entanto, quando eles percebem que não foram escolhidos apenas para ter sucesso, mas também por sua óbvia culpa quando inevitavelmente falharem, terão que decidir se vale a pena ou não continuar correndo risco de morte.”,
+Trailer: “https://youtu.be/8pYp4T8TdP4?si=j2qF23OdxO9f3VaQ”
+},
+{
+title: Infinito,
+genre: Ação,
+ageRating: 14 anos,
+synopsis:”Evan McCauley tem sua vida diária assombrada por habilidades que nunca aprendeu e memórias de lugares que nunca visitou. Automedicado e à beira de um ataque de nervos, Evan é contatado pelos "Infinitos", um grupo secreto que revela a ele que suas memórias podem ser reais, porém vindas de múltiplas vidas passadas.”,
+Trailer: “https://youtu.be/hvOeeNIKeuo?si=tZ3u2j4OLLzmcW3l”
+},
+{
+title: “Infiltrado”,
+genre: “Ação”,
+ageRating: 16 anos,
+synopsis: “Harry, conhecido apenas como H, é um homem misterioso que trabalha para uma empresa de carros-fortes e movimenta grandes quantias de dinheiro pela cidade de Los Angeles. Quando, ao impedir um assalto, ele surpreende a todos com suas habilidades de combate, suas verdadeiras intenções começam a ser questionadas e um plano maior é revelado.”,
+Trailer: “https://youtu.be/EFYEni2gsK0?si=S7uo2rtorGqU3dAE”
+},
+
+Comédia
+
+{
+title: “Partiu América”,
+genre: “Comédia”,
+ageRating: livre,
+synopsis: “Matheus é um dono de um parque de diversões muito simples, que tem o sonho de conhecer os famosos parques dos EUA. Mas, quando a família dele finalmente tem a oportunidade de viajar para o exterior pela primeira vez, as coisas não saem como esperado.”,
+Trailer:”https://youtu.be/1HS957R61Sk?si=R2HU--UFf43xbxZy”
+},
+{
+title: “Click”
+genre: "Comédia",
+ageRating: 10 anos,
+synopsis: "Um arquiteto casado e com filhos está cada vez mais frustrado por passar a maior parte de seu tempo trabalhando. Um dia, ele encontra um inventor excêntrico que lhe dá um controle remoto universal, com capacidade de acelerar o tempo. No início, ele usa o aparelho para acelerar qualquer momento tedioso, mas se dá conta de que está acelerando o tempo demais e deixando de viver preciosos momentos em família. Desesperado, ele procura o inventor para ajudá-lo a reverter o que fez.”,
+trailer:“https://youtu.be/iyjUxruK56s?si=PvaFmbV_f6hbPgZc”
+},
+{
+title: "Free Guy - Assumindo o Controle",
+genre: "Comédia",
+ageRating: 12 anos,
+synopsis: "Um caixa de banco preso a uma entediante rotina tem sua vida virada de cabeça para baixo quando descobre que é um personagem em um jogo interativo. Agora ele precisa aceitar sua realidade e lidar com o fato de que é o único que pode salvar o mundo.”,
+trailer:”https://youtu.be/iLcwgsQ0V28?si=_9twyAqB9HB_CTIh”
+},
+{
+title: "Antes Que Eu Me Esqueça",
+genre: "Comédia",
+ageRating: 14 anos,
+synopsis: "Aos 80 anos, Polidoro resolve demolir a estabilidade de sua confortável vida de juiz aposentado e virar sócio de uma boate de strip-tease. Beatriz, sua filha, decide interditá-lo judicialmente. Paulo, seu outro filho, se declara incapaz de opinar pois não mantém relações com o pai. O juiz determina o encontro forçado de pai e filho e a reaproximação transforma suas vidas.",
+trailer: "https://youtu.be/Yq2IXr-SryY?si=nTXtFPlA5UD2XN_t"
+},
+{
+title: "Tá Rindo do Quê?",
+genre: "Comédia",
+ageRating: 16 anos,
+synopsis: "George Simmons é um comediante de sucesso que descobre ter uma doença incurável e pouco menos de um ano de vida. Ira Wright também é comediante, possui um grande talento apesar de novato, mas trabalha em um restaurante. Certa noite, os dois artistas acabam se apresentando no mesmo local. George, imediatamente, decide ser o mentor de Ira para que ele possa dar continuidade ao seu trabalho. Nasce, então, uma amizade grande e genuína entre os dois comediantes.",
+trailer: "https://youtu.be/kzciY15Q3BA?si=rJvhSgYPuhZIeBr7"
+},
+{
+
+Drama 
+
+{
+title: "À Procura da Felicidade",
+genre: "Drama",
+ageRatings:livre,
+synopsis: "Chris enfrenta sérios problemas financeiros e sua esposa, Linda, decide partir. Agora solteiro, ele precisa cuidar de Christopher, seu filho de cinco anos. Chris tenta usar sua habilidade como vendedor para conseguir um emprego melhor, mas só consegue um estágio não-remunerado. Seus problemas financeiros não podem esperar uma promoção e eles acabam despejados. Chris e Christopher passam a dormir em abrigos ou onde quer que consigam um refúgio, mantendo a esperança de que dias melhores virão.",
+trailer: "https://youtu.be/yHgiEADa5M8?si=zpQoMMf3fAuPUkRZ"
+},
+{
+title: "Uma Vida de Esperança",
+genre: "Drama",
+ageRating: 10 anos,
+synopsis: "Uma cabeleireira em dificuldades encontra um renovado senso de propósito quando conhece um pai viúvo e suas duas filhas. Com o seu filho mais novo gravemente doente e à espera de um transplante de fígado, a mulher feroz reúne uma comunidade inteira para ajudar.",
+trailer: "https://youtu.be/I0-9Zr1rn-g?si=_LoJ8bnyV14Em1Ec"
+},
+{
+title: "Central do Brasil",
+genre: "Drama",
+ageRating:12 anos,
+synopsis: "Dora, uma amargurada ex-professora, ganha a vida escrevendo cartas para pessoas analfabetas, que ditam o que querem contar às suas famílias. Ela embolsa o dinheiro sem sequer postar as cartas. Um dia, Josué, o filho de nove anos de idade de uma de suas clientes, acaba sozinho quando a mãe é morta em um acidente de ônibus. Ela reluta em cuidar do menino, mas se junta a ele em uma viagem pelo interior do Nordeste em busca do pai de Josué, que ele nunca conheceu.",
+trailer: "https://youtu.be/RK0K3l-R-9w?si=AZxOsKjrH6hXYnCe"
+},
+{
+title: "12 Anos de Escravidão",
+genre: "Drama",
+ageRating: 14 anos,
+synopsis: "Em 1841, Solomon Northup é um negro livre, que vive em paz ao lado da esposa e filhos. Um dia, após aceitar um trabalho que o leva a outra cidade, ele é sequestrado e acorrentado. Vendido como se fosse um escravo, Solomon precisa superar humilhações físicas e emocionais para sobreviver. Ao longo de 12 anos, ele passa por dois senhores, Ford e Edwin Epps, que exploram seus serviços.",
+trailer:“https://youtu.be/xSL_sCHDsHc?si=8eWXGyI2CrwEuyav”
+},
+{
+title: "A baleia",
+genre: "Drama",
+ageRating: 16 anos ,
+synopsis: "Em A Baleia, seguimos um professor de inglês e seu relacionamento fragilizado com sua filha, Ellie. Charlie é um professor de inglês recluso, que vive com obesidade severa e luta contra um transtorno de compulsão alimentar. Ele dá aulas online, mas sempre deixa a webcam desligada, com medo de sua aparência. Apesar de viver sozinho, ele é cuidado pela sua amiga e enfermeira, Liz. Mesmo assim, ele é sozinho, convivendo diariamente apenas com a culpa, por ter abandonado Ellie, sua filha hoje adolescente que ele deixou junto com a mãe Mary, ao se apaixonar por um homem. Agora, ele irá buscar se reconectar com a filha adolescente e reparar seus erros do passado. Para isso, ele pede para que Ellie vá visitá-lo sem avisar sua mãe e ela aceita, com o única condição de que ele a ajuda a reescrever uma redação para a escola.",
+trailer: "https://youtu.be/GSUf-OKcDfg?si=TXXcnSzzNp2a23Gi”
+},
+{
+
+Animação
+
+{
+title: "Viva – A Vida é uma Festa",
+genre: "Animação",
+ageRating: livre,
+synopsis: "Apesar da proibição da música por gerações de sua família, o jovem Miguel sonha em se tornar um músico talentoso como seu ídolo Ernesto de la Cruz. Desesperado para provar seu talento, Miguel se encontra na deslumbrante e colorida Terra dos Mortos. Depois de conhecer um charmoso malandro chamado Héctor, os dois novos amigos embarcam em uma jornada extraordinária para desvendar a verdadeira história por trás da história da família de Miguel.",
+trailer: "https://youtu.be/iLmZZV-Nkuk?feature=shared"
+},
+{
+title: "Rango",
+genre: "Animação",
+ageRating: 10 anos,
+synopsis: "Um camaleão que viveu como um animal de estimação se encontra em uma crise de identidade. Rango se questiona sobre como se destacar quando é de sua natureza se camuflar. Acidentalmente, ele acaba em uma cidade de fronteira chamada Dirt e dá o primeiro passo em direção à transformação se tornando xerife. Embora inicialmente ele apenas tenha encenado, uma série de situações emocionantes e encontros escandalosos obrigam Rango a se tornar um verdadeiro herói.",
+trailer: "https://youtu.be/LwSfIbBH8Pw?si=lV5T3wgwY_cW3SCW"
+},
+{
+title: "Princesa Mononoke",
+genre: "Animação",
+ageRating: 12 anos,
+synopsis: "Um príncipe infectado por uma doença sabe que irá morrer a menos que encontre a cura. Sendo a sua última esperança, segue para o leste e, durante o caminho, encontra animais da floresta lutando contra a sua exploração, liderados pela princesa Mononoke.",
+trailer: "https://youtu.be/4OiMOHRDs14?si=cmQUDSF8jFwOl-rQ"
+},
+{
+title: "Jovens Titãs - O Contrato de Judas",
+genre: "Animação",
+ageRating: 14 anos,
+synopsis: "Conduzido por Starfire, os Teen Titãs formam uma equipe compacta em sua batalha contra o mal. Mas seu mais novo parceiro, o poderoso Terra, pode alterar essa dinâmica.",
+trailer: "https://youtu.be/SmT8g_zwprg?si=mTOMcD6Kl3N5QwB8"
+},
+{
+title: "Mars Express",
+genre: "Animação",
+ageRating: 16 anos,
+synopsis: "Uma detetive particular obstinada e um androide, réplica de seu parceiro morto, encaram uma perigosa missão em Marte. O objetivo é encontrar uma jovem hacker, detentora de um segredo obscuro sobre os robôs, antes que seus perseguidores a capturem.",
+trailer: "https://youtu.be/SUMdLX4q1Dc?si=XqJ_D4utffvXZYrh"
+},
+{
+
+Suspense 
+
+{
+title: "Frankenweenie",
+genre: "Suspense",
+ageRating: livre,
+synopsis: "Victor adora fazer filmes caseiros de terror, quase sempre estrelados por seu cachorro Sparky. Quando o cão morre atropelado, o garoto fica triste e inconformado. Inspirado por uma aula de ciências que teve na escola, onde um professor mostra ser possível estimular os movimentos através da eletricidade, ele constrói uma máquina que permita reviver Sparky. O experimento dá certo, mas o que Victor não esperava era que seu melhor amigo voltasse com hábitos um pouco diferentes.",
+trailer: "https://youtu.be/_GhG8fE-X6M?si=e275e1pSTLKsoEVn"
+},
+{
+title: "O Turista",
+genre: "",
+ageRating: 10 anos,
+synopsis: "Durante uma viagem improvisada à Europa para curar um coração partido, o professor de matemática Frank Tupelo se vê em uma situação extraordinária quando uma estranha sedutora, Elise, cruza seu caminho. O flerte aparentemente inocente se transforma em um perigoso jogo de gato e rato.",
+trailer:"https://youtu.be/SPHqZ1UZukA?si=IUx3VVf-fYizuOAM"
+},
+{
+title: "O Impossível",
+genre: "Suspense",
+ageRating: 12 anos,
+synopsis: "O casal Maria e Henry está aproveitando as férias de inverno na Tailândia junto com os três filhos pequenos. Mas em uma manhã, um tsunami de proporções devastadoras atinge o local, arrastando tudo o que encontra pela frente. Separados em dois grupos, a mãe e o filho mais velho vão enfrentar situações desesperadoras para se manterem vivos, enquanto, o pai e as duas crianças menores não sabem se os outros dois ainda estão vivos.",
+trailer:"https://youtu.be/N6W5jbWq_h0?si=0M3qumyPRlps0Uu-"
+},
+{
+title: "Número 23",
+genre: "Suspense",
+ageRating: 14 anos,
+synopsis: "Um homem encontra um livro obscuro sobre o número 23 e inicia uma jornada sombria. À medida que ele se torna cada vez mais obcecado com o conteúdo, ele se convence de que o livro é baseado em sua vida. Para o seu desespero, ele descobre ainda que graves consequências estão armazenadas para o principal personagem da obra.",
+trailer: "https://youtu.be/erWwBKcRc5I?si=F84zGbjQACExjtMk"
+},
+{
+title: "A Teia",
+genre: "Suspense",
+ageRating: 16 anos,
+synopsis: "Sofrendo de perda de memória, um ex-detetive de homicídios tenta resolver um assassinato brutal do qual não consegue se lembrar. À medida que reúne evidências de uma investigação de uma década, ele logo descobre segredos em seu passado esquecido.",
+trailer: "https://youtu.be/W4T9rZq7bf8?si=gO09LWLP58uM64tX"
+},
+
+Terror
+
+{
+title: "A Família Addams",
+genre: "Terror",
+ageRating: livre,
+synopsis: "A família Addams se prepara para a visita de parentes ainda mais assustadores, mas a sombria personalidade da TV local Margaux Needler aparece para fazer uma fortuna vendendo todas as casas do bairro. Como resultado, os dias de maldade da mansão no topo da colina, que Margaux despreza, parece ter seus dias contados.",
+trailer: "https://youtu.be/v_6oiWUvUus?si=-g7nHcWHfNTbCBe6"
+},
+{
+title: "Coraline e o Mundo Secreto",
+genre: "Terror",
+ageRating: 10 anos,
+synopsis: "Enquanto explora sua nova casa à noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos têm botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com essa descoberta, mas logo percebe que segredos estranhos estão em ação: uma outra mãe e o resto de sua família tentam mantê-la eternamente nesse mundo paralelo.",
+trailer:"https://youtu.be/X3Pc9emMSxk?feature=shared"
+},
+{
+title: "Os Outros",
+genre: "Terror",
+ageRating: 12 anos,
+synopsis: "Durante a Segunda Guerra, a devota Grace aguarda com os filhos o retorno do marido dos campos de batalha. Isolados em uma mansão numa ilha deserta, passam a observar estranhos acontecimentos ao seu redor.",
+trailer:"https://youtu.be/iZmV5gOwHRI?si=y9bwHhb9Tz3gQiCg"
+},
+{
+title: "Medo Profundo",
+genre: "Terror",
+ageRating: 14 anos,
+synopsis: "Presas nas profundezas do oceano e cercadas por tubarões-brancos, duas irmãs precisam lutar contra o tempo para permanecer vivas. No entanto, elas têm apenas uma hora de oxigênio e as chances se tornam cada vez menores com os predadores rondando.",
+trailer:"https://youtu.be/GjdEv2d3zC0?si=6bvS01D14oMiLtQS"
+},
+{
+title: "Terror na Estrada",
+genre: "Terror",
+ageRating: 16 anos ,
+synopsis: "Mallory, uma jovem noiva, decide dar carona a um desconhecido depois que ele a ajuda quando seu carro quebra. O que ela não esperava, todavia, era que esse desconhecido se revelasse um psicopata. Percebendo que ele não está usando cinto de segurança, Mallory tenta matá-lo acelerando em direção a um parapeito. Mas ela acaba presa no carro e, agora, para conseguir sair viva dessa perigosa situação, a jovem precisa tomar uma atitude bastante drástica.",
+trailer: "https://youtu.be/_sQ7Kios8AY?si=crKXJmDY461sW0zp”
+},
+
+Ficção científica 
+
+{
+title: "E.T. O Extraterrestre",
+genre: "Ficção científica",
+ageRating: livre,
+synopsis: "O garoto Elliott faz amizade com um pequeno alienígena inofensivo que está bem longe do seu planeta. Ele decide manter a adorável criatura em segredo e em casa após apresentá-la aos irmãos.",
+trailer: "https://youtu.be/FkT-LM8JxCs?si=yg7Jx3c04zC0sH-1"
+},
+{
+title: "Interestelar",
+genre: "Ficção científica",
+ageRating: 10 anos,
+synopsis: "As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar.",
+trailer:"https://youtu.be/i6avfCqKcQo?si=UvnNkFVxKgJVt4v9"
+},
+{
+title: "65 - Ameaça Pré-Histórica",
+genre: "Ficção científica",
+ageRating: 12 anos,
+synopsis: "Após um acidente catastrófico em um planeta desconhecido, o piloto Mills descobre que, na verdade, está preso na Terra 65 milhões de anos atrás. Agora, com apenas uma chance de resgate, Mills e a única outra sobrevivente, Koa, enfrentam criaturas",
+trailer:"https://youtu.be/FDeGlDD9pk8?si=cYXR2oH0bJfd5qCY"
+},
+{
+title: "A Origem",
+genre: "Ficção científica",
+ageRating: 14 anos,
+synopsis: "Dom Cobb é um ladrão com a rara habilidade de roubar segredos do inconsciente, obtidos durante o estado de sono. Impedido de retornar para sua família, ele recebe a oportunidade de se redimir ao realizar uma tarefa aparentemente impossível: plantar uma ideia na mente do herdeiro de um império. Para realizar o crime perfeito, ele conta com a ajuda do parceiro Arthur, o discreto Eames e a arquiteta de sonhos Ariadne. Juntos, eles correm para que o inimigo não antecipe seus passos.",
+trailer:"https://youtu.be/R_VX0e0PX90?si=0pwaSM6rEEeZ9_fG" 
+},
+{
+title: "Extinção",
+genre: "Ficção científica",
+ageRating: 16 Anos,
+synopsis: "Um homem é atormentado por pesadelos com uma invasão alienígena e suas visões logo se tornam realidade quando uma força extraterrestre ataca o planeta. Assim, ele deve encontrar um lugar seguro para sua família em meio a um mundo em ruínas.",
+trailer:"https://youtu.be/R_VX0e0PX90?si=0pwaSM6rEEeZ9_fG"
+},
+
+Aventura 
+
+{
+title: "Os Caçadores da Arca Perdida",
+genre: "Aventura",
+ageRating: livre,
+synopsis: "O arqueólogo Indiana Jones precisa encontrar a Arca da Aliança, uma relíquia bíblica que contém os dez mandamentos. Como o portador do artefato se torna invencível, os nazistas também vão fazer de tudo para adquirir esse precioso objeto.",
+trailer:"https://youtu.be/G_wcEapEkpM?si=esL7Oj-vVYC3JBMg"
+},
+{
+title: "Jack, o Caçador de Gigantes",
+genre: "Aventura",
+ageRating: 10 anos,
+synopsis: "Jack é um pobre fazendeiro que abre um portal entre o mundo real e o universo de uma terrível raça de gigantes, desencadeando uma guerra. A situação obriga Jack a lutar para deter os gigantes e salvar a raça humana.",
+trailer: "https://youtu.be/Z121wFs-Oe0?si=qtk0vL7MidBgBF7W" 
+},
+{
+title: "Jurassic Park - O Parque dos Dinossauros",
+genre: "Aventura",
+ageRating: 12 anos,
+synopsis: "Os paleontólogos Alan Grant, Ellie Sattler e o matemático Ian Malcolm fazem parte de um seleto grupo escolhido para visitar uma ilha habitada por dinossauros criados a partir de DNA pré-histórico. O idealizador do projeto e bilionário John Hammond garante a todos que a instalação é completamente segura. Mas após uma queda de energia, os visitantes descobrem, aos poucos, que vários predadores ferozes estão soltos e à caça.",
+trailer: "https://youtu.be/yO8DP21B9-U?si=P4RvWTB1HGvsLXmG"
+},
+{
+title: "Na Selva",
+genre: "Aventura",
+ageRating: 10,
+synopsis: "Um misterioso guia acompanha um aventureiro e seu amigo na floresta amazônica. A jornada se torna aterrorizante à medida que os elementos mais sombrios da natureza humana e as mais mortíferas ameaças os forçam a lutar pela sobrevivência.",
+trailer:"https://youtu.be/vQaLraAr8OY?si=h2nzWqtYtaxzl29M"
+},
+{
+title: "O Poder e o Impossível",
+genre: "Aventura",
+ageRating: 16 anos,
+synopsis: "Isolado em uma estação de esqui, o ex-jogador de hóquei Eric LeMarque é pego de surpresa por uma tempestade de neve. Sozinho e sem comunicação, ele embarca em uma jornada de autoconhecimento e fé enquanto luta para sobreviver.",
+trailer:"https://youtu.be/d58BmYIssQk?si=610g_1nu8_C4VmCN"
+},
+
